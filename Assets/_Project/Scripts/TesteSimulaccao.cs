a50c047f815a65276bf47e8415232a280b7f43dc
MartinsHenrique <martisnhenrique00@gmail.com>
2026-09-07 19:14:17 -0300

    base 2

--- Assets/_Project/Scripts/TesteSimulaccao.cs
+++ Assets/_Project/Scripts/TesteSimulaccao.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class TesteSimulaccao : MonoBehaviour
 {
-    [SerializeField] private NPC npc;
+    [SerializeField] private List<NPC> npc = new List<NPC>();
     
     [SerializeField] private List<NPCStatusSO> npcStatus = new List<NPCStatusSO>();
     [SerializeField] private List<NPCActionSO> npcAction = new List<NPCActionSO>();
